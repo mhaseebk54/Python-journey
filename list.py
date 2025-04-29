777aebb35acb9d47a8b9a35250b8dd599945c305
Muhammad Haseeb Khalid <m.haseebk54@gmail.com>
2025-04-29 23:04:47 +0500

    Add files via upload

---
+++ list.py
@@ -0,0 +1,30 @@
+fruits = ["apple","banana","mango","watermelon","grapes"]
+vegetable = ["potato","tomato","onion"]
+print(fruits)
+
+# access item
+print(fruits[2])
+
+# replace item
+fruits[1] = "orange"
+print(fruits)
+
+# add item
+fruits.append("cherry")
+print(fruits)
+
+# remove item
+fruits.remove("grapes")
+print(fruits)
+
+# sorts item
+fruits.sort
+print(fruits)
+
+# copy item
+fruits.copy()
+print(fruits)
+
+# add list
+add = fruits + vegetable
+print(add)
