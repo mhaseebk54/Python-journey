6ed122b6cde4acee7de4382969affee5dfe4abae
Muhammad Haseeb Khalid <m.haseebk54@gmail.com>
2025-04-29 22:55:38 +0500

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,16 @@
+fruits = {"apple","banana","mango","watermelon","grapes"}
+print(fruits)
+
+# acess item 
+print("apple" in fruits)
+
+print("apple" not in fruits)
+
+# Add Item
+fruits.add("orange")
+print(fruits)
+
+# Remove Item
+fruits.remove("banana")
+print(fruits)
+
